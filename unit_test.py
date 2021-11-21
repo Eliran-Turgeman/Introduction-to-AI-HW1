ab82544a0b30a31808c9354ebf0b54037d936793
Dor Noti <dorn@ip-10-1-145-10>
2021-11-21 23:06:06 +0200

    updated unit_test

--- unit_test.py
+++ unit_test.py
@@ -1,33 +1,47 @@
 from MazeProblem import MazeProblem, create_problem
-from Robot import BreadthFirstSearchRobot as BFS, UniformCostSearchRobot
+from Robot import BreadthFirstSearchRobot as BFS, UniformCostSearchRobot, BestFirstSearchRobot
 from Animation import Animation
 import sys
-SHOW_ANIMATION = False
+import argparse
 
 EXPECTED_RESULTS = {'bfs': [(36, 57), (51, 362), (216, 1212), (88, 2430), (123, 1209), (376, 5299)],
                     'ucs': [(36, 51), (47, 312), (216, 1212), (84, 2447), (123, 1205), (376, 5299)]}
 
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--index', type=str, default=None, help="maze index")
+    parser.add_argument('-m', '--methods', type=str, default='all', help="search methods (separated by ,")
+    parser.add_argument('--animate', action='store_true')
+    return parser.parse_args()
 
-def main_test(test_index):
+
+def main_test(test_index, methods, animate):
     maze_name = 'maze_' + str(test_index)
     maze_problem = create_problem(maze_name)
 
-    bfs = BFS()
-    ucs = UniformCostSearchRobot()
-    bfs_solution = bfs.solve(maze_problem)
-    ucs_solution = ucs.solve(maze_problem)
 
-    search_algorithms = [bfs, ucs]
-    solutions = [bfs_solution, ucs_solution]
+    solutions = {}
+    if 'bfs' in methods:
+        bfs = BFS()
+        bfs_solution = bfs.solve(maze_problem)
+        solutions['bfs'] = {'algorithm': bfs, 'solution': bfs_solution}
 
-    for solution, key in zip(solutions, EXPECTED_RESULTS.keys()):
+    if 'ucs' in methods:
+        ucs = UniformCostSearchRobot()
+        ucs_solution = ucs.solve(maze_problem)
+        solutions['ucs'] = {'algorithm': ucs, 'solution': ucs_solution}
+
+    for key, search_alg_data in solutions.items():
+        solution = search_alg_data['solution']
         assert EXPECTED_RESULTS[key][int(test_index)][0] == solution.cost, f"cost equals {solution.cost} in test {test_index} of {key}"
         assert EXPECTED_RESULTS[key][int(test_index)][1] == solution.n_node_expanded, f"node expanded equals {solution.n_node_expanded} in test {test_index} of {key}"
         print(f"TEST {test_index} for {key} PASSED")
 
-    if SHOW_ANIMATION:
+    if animate:
         try:
-            for search_algo, solution in zip(search_algorithms, solutions):
+            for key, search_alg_data in solutions.items():
+                solution = search_alg_data['solution']
+                search_algo = search_alg_data['algorithm']
                 animation = Animation(solution, maze_problem, search_algo,
                                       maze_name, blit=False)
                 animation.show()
@@ -35,21 +49,27 @@ def main_test(test_index):
             print('No solution!')
 
 
-def test_wrapper():
+def test_wrapper(args):
+    if args.methods == 'all':
+        methods = [k for k in EXPECTED_RESULTS.keys()]
+    else:
+        methods = args.methods.split(',')
+
     test_index = -1
-    try:
-        test_index = sys.argv[1]
-    except IndexError:
+    if args.index is None:
         print("Running all tests")
+    else:
+        test_index = args.index
 
     if test_index == -1:
         for test_idx in range(0, 6):
-            main_test(test_idx)
+            main_test(test_idx, methods, animate=args.animate)
     else:
-        main_test(test_index)
+        main_test(test_index, methods, animate=args.animate)
 
 
 if __name__ == '__main__':
-    test_wrapper()
+    args = parse()
+    test_wrapper(args)
 
 
