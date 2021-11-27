ce2dbba1f61bd2b99a9ad40794ee4eecb183ffe7
Dor Noti <dorn@ip-10-1-145-10>
2021-11-27 22:05:55 +0200

    removed pdb

--- Heuristics.py
+++ Heuristics.py
@@ -70,8 +70,6 @@ class ShorterRobotHeuristic:
             node = self.node_dists.get_node(new_state)
             return node.g_value
         else:
-            import pdb;
-            pdb.set_trace()
             return center_manhattan_heuristic(state)
             # what should we return in this case, so that the heuristic would be as informative as possible
                         # but still admissible
